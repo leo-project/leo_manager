4c2fcf8cb41b2dcb90887f5e75cfd1a0c8b39a28
yosukehara <yousuke.hara@gmail.com>
2012-11-26 18:14:52 +0900

    Fix a auth-related bug

--- src/leo_manager_console.erl
+++ src/leo_manager_console.erl
@@ -722,7 +722,7 @@ s3_create_key(CmdBody, Option) ->
 s3_get_users(CmdBody) ->
     _ = leo_manager_mnesia:insert_history(CmdBody),
 
-    case leo_s3_users:find_users_all() of
+    case leo_s3_user:find_users_all() of
         {ok, Users} ->
             {ok, Users};
         Error ->
