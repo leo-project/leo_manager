8cf34349fbf99d0409992ec9ae76503103f324d1
Yosuke Hara <yousuke.hara@gmail.com>
2014-12-03 18:16:53 +0900

    Fix incorrect status

--- src/leo_manager_cluster_monitor.erl
+++ src/leo_manager_cluster_monitor.erl
@@ -333,7 +333,7 @@ code_change(_OldVsn, State, _Extra) ->
              ok | delete | {error, any()}).
 update_node_state(start, ?STATE_ATTACHED, _Node) -> ok;
 update_node_state(start, ?STATE_DETACHED, _Node) -> ok;
-update_node_state(start, ?STATE_SUSPEND,   Node) -> update_node_state_1(?STATE_RESTARTED, Node);
+update_node_state(start, ?STATE_SUSPEND,   Node) -> ok;
 update_node_state(start, ?STATE_RUNNING,  _Node) -> ok;
 update_node_state(start, ?STATE_STOP,      Node) -> update_node_state_1(?STATE_RESTARTED, Node);
 update_node_state(start, ?STATE_RESTARTED,_Node) -> ok;
